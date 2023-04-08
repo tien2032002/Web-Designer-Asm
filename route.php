9ab022ad25d3c15bb365b8c4e2e0ef9ca0ef1af6
tien2032002 <80495035+tien2032002@users.noreply.github.com>
2023-04-08 15:05:24 +0700

    add menu

--- route.php
+++ route.php
@@ -1,8 +1,9 @@
 <?php
     //list of controllers and action
-    $controllers = array('guest' => ['home_page', 'login', 'signup', 'login_manager'],
+    $controllers = array('guest' => ['home_page', 'login', 'signup', 'login_manager', 'dish_list'],
                          'user' => ['home_page_user', 'signup', 'login', 'logout', 'profile_user'],
-                         'manager' => ['home_page_manager', 'manage_employee', 'login', 'logout', 'add_employee', 'delete_employee', 'update_employee'],
+                         'manager' => ['home_page_manager', 'manage_employee', 'login', 'logout', 'add_employee',
+                                       'delete_employee', 'update_employee'],
                          'employee');
     //if controller or action not in above list, go to error page
     if (!array_key_exists($controller, $controllers) || !in_array($action, $controllers[$controller])) {
