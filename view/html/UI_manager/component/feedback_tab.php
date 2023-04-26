94b791386f96c2f6d81f0b8e3d0ebe9b82f4e6d3
minhle106 <minhletravel106@gmail.com>
2023-04-26 22:43:20 +0700

    Update_feedback_tab

---
+++ view/html/UI_manager/component/feedback_tab.php
@@ -0,0 +1,68 @@
+<div class="row d-flex justify-content-center mt-4 me-4 ms-4">
+    <div class="card text-dark">
+        <h4 class="mt-3 ms-3">Recent comments</h4>
+        <hr class="mt-0">
+        <div class="card-body d-flex align-items-center justify-content-between">
+            <div class="d-flex flex-start">
+                <img class="rounded-circle me-3"
+                src="view/images/avt_user.jpg" alt="avatar" width="60"
+                height="60">
+                <div>
+                    <h6 class="fw-bold mb-1">Minh Lee</h6>
+                    <div class="d-flex align-items-center mb-3">
+                        <p class="mb-0 me-4 text-muted">March 07, 2021</p>
+                        <span style="font-weight:500;">Rating: 4.5<i style="color: #da5619;" class="bi bi-star-fill ms-1"></i></span>
+                    </div>
+                    <div class="mb-0 col-11">
+                        Lorem Ipsum is simply dummy text
+                        Lorem Ipsum is simply dummy text
+                        Lorem Ipsum is simply dummy text
+                    </div>
+                </div>
+            </div>
+            <button class="btn btn-danger" style="border-radius: 0;">Xóa</button>
+        </div>
+        <hr class="mt-0">
+        <div class="card-body d-flex align-items-center justify-content-between">
+            <div class="d-flex flex-start">
+                <img class="rounded-circle me-3"
+                src="view/images/avt_user.jpg" alt="avatar" width="60"
+                height="60">
+                <div>
+                    <h6 class="fw-bold mb-1">Minh Lee</h6>
+                    <div class="d-flex align-items-center mb-3">
+                        <p class="mb-0 me-4 text-muted">March 07, 2021</p>
+                        <span style="font-weight:500;">Rating: 4.5<i style="color: #da5619;" class="bi bi-star-fill ms-1"></i></span>
+                    </div>
+                    <div class="mb-0 col-11">
+                        Lorem Ipsum is simply dummy text
+                        Lorem Ipsum is simply dummy text
+                        Lorem Ipsum is simply dummy text
+                    </div>
+                </div>
+            </div>
+            <button class="btn btn-danger" style="border-radius: 0;">Xóa</button>
+        </div>
+        <hr class="mt-0">
+        <div class="card-body d-flex align-items-center justify-content-between">
+            <div class="d-flex flex-start">
+                <img class="rounded-circle me-3"
+                src="view/images/avt_user.jpg" alt="avatar" width="60"
+                height="60">
+                <div>
+                    <h6 class="fw-bold mb-1">Minh Lee</h6>
+                    <div class="d-flex align-items-center mb-3">
+                        <p class="mb-0 me-4 text-muted">March 07, 2021</p>
+                        <span style="font-weight:500;">Rating: 4.5<i style="color: #da5619;" class="bi bi-star-fill ms-1"></i></span>
+                    </div>
+                    <div class="mb-0 col-11">
+                        Lorem Ipsum is simply dummy text
+                        Lorem Ipsum is simply dummy text
+                        Lorem Ipsum is simply dummy text
+                    </div>
+                </div>
+            </div>
+            <button class="btn btn-danger" style="border-radius: 0;">Xóa</button>
+        </div>
+    </div>
+</div>
